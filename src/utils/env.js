98ec39e174902e3a3446c102d289a38051c90a5e
Alona Tiunina <163348743+tiunina@users.noreply.github.com>
2024-12-14 14:59:04 +0200

    task1-2

--- src/utils/env.js
+++ src/utils/env.js
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-export function env(name, defaultValue) {
+export const env = (name, defaultValue) => {
   const value = process.env[name];
 
   if (value) return value;
@@ -10,4 +10,4 @@ export function env(name, defaultValue) {
   if (defaultValue) return defaultValue;
 
   throw new Error(`Missing: process.env['${name}'].`);
-}
+};
