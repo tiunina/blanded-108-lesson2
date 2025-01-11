f7821e4ba4d6184b00c2e1efb9ade1e866835106
Liza <onishenkolizka99@gmail.com>
2025-01-11 15:21:31 +0200

    login

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,1 +1,4 @@
 export const categoryList = ['books', 'electronics', 'clothing', 'other'];
+
+export const FIFTEEN_MINUTES = 15 * 60 * 1000;
+export const ONE_DAY = 24 * 60 * 60 * 1000;
--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,6 +1,7 @@
 import { HttpError } from 'http-errors';
 
 export const errorHandler = (error, req, res, next) => {
+  console.log('error:', error);
   if (error instanceof HttpError) {
     res.status(error.status).json({
       status: error.status,
